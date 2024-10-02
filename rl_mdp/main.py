6e36dfe62f5df11cd5df4b521d3b714a08d05f14
ssvosss <152398129+ssvosss@users.noreply.github.com>
2024-10-02 19:58:05 +0200

    Add files via upload

--- rl_mdp/main.py
+++ rl_mdp/main.py
@@ -1,8 +1,18 @@
+import util
+import model_free_prediction.monte_carlo_evaluator as mce
+
+
 def main() -> None:
     """
     Starting point of the program, you can instantiate any classes, run methods/functions here as needed.
     """
-    pass
+    mdp = util.create_mdp()
+    policy_1 = util.create_policy_1()
+    policy_2 = util.create_policy_2()
+
+    mc_evaluator = MCEvaluator(mdp)
+    mc_evaluator.evaluate(policy_1, 5)
+
 
 
 if __name__ == "__main__":
